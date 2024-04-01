a46844868124f9ed4ec901552a2ec5d66bc3ea5a
zaheem hassan <164178436+zaheem55@users.noreply.github.com>
2024-04-01 20:22:38 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,29 @@
+import inquirer from "inquirer"
+
+let todoList = [];
+let conditions = true;
+
+console.log("wellcome to Todo-List Application");
+
+while(conditions){
+    let addTask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: "Ener your New Task :"
+        }
+    ]);
+    todoList.push(addTask.task);
+    console.log(`${addTask.task} Task added in Todo-list successfully`);
+
+    let addMoreTask = await inquirer.prompt([
+        {
+            name: "addmore",
+            type: "confirm",
+            message: "Do you want to add more task ?",
+            default: "False"
+        }
+    ]);
+    conditions = addMoreTask.addmore
+}
+console.log(`Your updated Todo-List : ${todoList}`);
